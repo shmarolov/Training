f325004681182f7d682c26803dceb6fdf9e68962
shmarolov <beglanesirkepov@gmail.com>
2022-09-04 20:48:24 +0600

    Добавьте файлы проекта.

---
+++ Training/ArithmeticTasks.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training
+{
+    internal static class ArithmeticTasks
+    {
+        //first task
+        internal static int sumF(int a, int b)
+        {
+            int sum = a + b;
+            return sum;
+        }
+    }
+}
---
+++ Training/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Training;
+
+class Train
+{
+    static void Main(string[] args)
+    {
+        List<string> answers = new List<string>();
+        answers.Append(ArithmeticTasks.sumF(5, 1).ToString());
+
+        foreach (string Answers in answers)
+        {
+            Console.WriteLine($"{Answers}") ;
+        }
+    }
+}
